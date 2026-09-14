08b8661b02f006fa1504c6b23408f4912450deb1
Luthfiyah Putri Aliati <2410631250097@student.unsika.ac.id>
2026-09-14 22:15:33 +0700

    Add MVC implementation for books categories and members

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MemberController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/books', [BookController::class, 'index']);
+
+Route::get('/categories', [CategoryController::class, 'index']);
+
+Route::get('/members', [MemberController::class, 'index']);
